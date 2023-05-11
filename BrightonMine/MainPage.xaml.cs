066c4c6a3f37fed7d2ebb336e29efa488cfc3a1e
kazusapon <flum1278pool@gmail.com>
2023-05-12 01:48:36 +0900

    プロジェクト ファイルを追加します。

---
+++ BrightonMine/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BrightonMine;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ BrightonMine/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace BrightonMine;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
